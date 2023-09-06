882d0a895a79879d11ba9d547e0b4fda5ef15b90
Steven Massaro <steven.massaro.web@gmail.com>
2023-09-06 14:59:08 -0500

    add some sleeps to startup script

--- startup.sh
+++ startup.sh
@@ -3,10 +3,16 @@
 # start cupsd and run it detached from the current console
 /usr/sbin/cupsd
 
+sleep 5s
+
 # install the new printer (this needs cupsd running)
 expect ./install-printer.exp
 
+sleep 5s
+
 # kill cupsd
 pkill cupsd
+
+sleep 5s
 
 /entrypoint.sh
