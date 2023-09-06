d1031ba0a455cad2ef8e7b47c8b65683c3abb684
Steven Massaro <steven.massaro.web@gmail.com>
2023-09-06 15:06:14 -0500

    support starting from a fresh install

--- startup.sh
+++ startup.sh
@@ -1,18 +1,14 @@
 #!/bin/bash -ex
 
 # start cupsd and run it detached from the current console
-/usr/sbin/cupsd
+/entrypoint.sh &
 
 sleep 5s
 
 # install the new printer (this needs cupsd running)
 expect ./install-printer.exp
 
-sleep 5s
-
 # kill cupsd
 pkill cupsd
-
-sleep 5s
 
 /entrypoint.sh
